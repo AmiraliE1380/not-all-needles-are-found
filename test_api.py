54495176372f8db79c028aa03adf44a2edc70129
AmiraliE1380 <ebrahimzadeh.amirali@gmail.com>
2025-07-15 11:07:30 -0400

    Established connection via OpenAI API.

--- test_api.py
+++ test_api.py
@@ -11,6 +11,31 @@ client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
 # Example request
 response = client.chat.completions.create(model="gpt-4o",
-messages=[{"role": "user", "content": "Hello! How long can the context of famous LLMs like latest versions of OpenAI models, or Gemini's latest model be? Tell me how many tokens? Please search if you don't have the information."}])
+messages=[{"role": "user",
+           "content": "Hello! How long can the context of famous LLMs like latest versions of OpenAI models, or Gemini's latest model be? Tell me how many tokens? Please search if you don't have the information."
+           }])
 
 print(response.choices[0].message.content)
+
+
+openai_models = [
+    "gpt-4o",
+    "gpt-4o-mini",
+    "gpt-4-turbo-2024-04-09",
+    "gpt-4.1",
+    "gpt-4.1-mini",
+    "gpt-4.1-nano",
+    "gpt-4-0613",
+    "gpt-4-32k-0613",
+    "gpt-3.5-turbo",
+    "gpt-3.5-turbo-0125",
+    "gpt-3.5-turbo-0613",
+    "gpt-3.5-turbo-16k",
+    "gpt-3.5-turbo-instruct",
+    "o1",
+    "o1-mini",
+    "o1-pro",
+    "o3-mini",
+    "o3",
+    "o3-pro"
+]
