2dbae62bcfc45483869b2b52176571cab9ff0bba
AmiraliE1380 <ebrahimzadeh.amirali@gmail.com>
2025-12-19 12:22:50 -0500

    Created corpus for Claude and Gemini.

--- counter.py
+++ counter.py
@@ -47,4 +47,4 @@ if __name__ == "__main__":
 
     # count_words_and_tokens("texts/la_comédie_humaine_(balzac)/contracted/la_comédie_humaine_expected_128000_actual_154955.txt")
     # count_words_and_tokens("texts/la_comédie_humaine_(balzac)/contracted/gpt/la_comédie_humaine_400k_expected_10%.txt")
-    count_words_and_tokens("texts/la_comédie_humaine_(balzac)/contracted/gpt/la_comédie_humaine_expected_200000_actual_209885.txt")
+    count_words_and_tokens("texts/la_comédie_humaine_(balzac)/contracted/gpt/la_comédie_humaine_272k_expected_40%.txt")
