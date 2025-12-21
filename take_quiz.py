b7137d77c0ed3ed605341d6698317d7097d0de64
AmiraliE1380 <ebrahimzadeh.amirali@gmail.com>
2025-12-20 19:18:46 -0500

    Generated larges corpus for Claude.

--- take_quiz.py
+++ take_quiz.py
@@ -112,7 +112,7 @@ def take_quizes_diff_lengths():
     # for i in [6]:
     # for i in range(10):
     # for i in range(8,10):
-    for i in [2]:
+    for i in [6]:
         story_address = f"texts/la_comédie_humaine_(balzac)/contracted/gpt/la_comédie_humaine_{max_context_length}k_expected_{(i+1)*10}%.txt"
         # grades.append([])
         # for j in [9]:
@@ -121,7 +121,7 @@ def take_quizes_diff_lengths():
                                     #   "",
                                       "_no_hallucination"
                                       ]:
-            for j in range(0,10):
+            for j in range(5,10):
             # for j in [9]:
                 fact_location = j * 0.1 + 0.05
                 print(f"Taking quiz for story length {(i+1)*10}% and fact location {fact_location*100:.0f}...")
@@ -149,7 +149,7 @@ def take_quizes_diff_lengths():
                 print(f"response: {response}\n")
                 print("\n" + "="*50 + "\n")
 
-                time.sleep(150)  # to avoid token rate per minute limit errors
+                time.sleep(350)  # to avoid token rate per minute limit errors
         
 
                 # save_results_path = f"logs/quiz_responses_{id}_{model}.txt"
